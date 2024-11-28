2706e07063d3349bacb076593e7d7b7ca9765231
Wayne Maurer <2899448+wmaurer@users.noreply.github.com>
2024-11-28 23:27:50 +0100

    Added interpolation feature and performance improvements

--- apps/op2env/src/assets/op2env.fish
+++ apps/op2env/src/assets/op2env.fish
@@ -1,15 +1,18 @@
 function op2env
     set filename $argv[1]
 
+    # Run op2env-print with all arguments and capture its output
     set output (op2env-print $argv)
     if test $status -ne 0
         return $status
     end
 
+    # Split the output into lines
     set lines (string split "\n" $output)
 
+    # Process each line
     for i in (seq (count $lines))
-        set parts (string split '=' $lines[$i])
-        set -gx $parts[1] $parts[2]
+        set parts (string match -r '([^=]+)=(.*)' $lines[$i])
+        set -gx $parts[2] $parts[3]
     end
 end
--- apps/op2env/src/op2env-print.ts
+++ apps/op2env/src/op2env-print.ts
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
-import { Args, Command } from '@effect/cli'
+import { Args, Command, Options } from '@effect/cli'
 import { FileSystem } from '@effect/platform'
 import { NodeContext, NodeRuntime } from '@effect/platform-node'
-import { Array, Console, Effect, Order, String, Tuple, pipe } from 'effect'
+import { Array, Console, Effect, Random, String, Tuple, pipe } from 'effect'
 
 import * as Op from './Op.js'
+
+const splitByFirstEqualSign = (s: string) => {
+    const index = s.indexOf('=')
+    if (index === -1) return Tuple.make(s, '')
+    return Tuple.make(s.slice(0, index), s.slice(index + 1))
+}
 
 const getEntriesFromFile = (file: string) =>
     Effect.gen(function* () {
@@ -13,37 +19,136 @@ const getEntriesFromFile = (file: string) =>
         const contents = yield* fs.readFileString(file)
         return pipe(
             Array.filter(contents.split('\n'), String.isNonEmpty),
-            Array.map((s) => s.split('=')),
+            Array.filter((s) => !s.startsWith('#')),
+            Array.map(splitByFirstEqualSign),
             Array.filter(Tuple.isTupleOf(2)),
         )
     })
 
-const program = (files: string[]) =>
+const extractInterpolatedVariables = (s: string) => {
+    const matches = s.match(/\$\{([^}]+)\}/g)
+    if (matches === null) return []
+    return matches.map((m) => m.slice(2, m.length - 1))
+}
+
+const program = (files: string[], exprt: boolean) =>
     Effect.gen(function* () {
         const entries = yield* pipe(Array.map(files, getEntriesFromFile), Effect.all, Effect.map(Array.flatten))
 
-        const environmentVariablesNames = new Set(Array.map(entries, (e) => e[0]))
+        const id = yield* Random.nextInt
+        const entriesWithInterpolatedVariables = pipe(
+            entries,
+            Array.map(([key, value]) => ({
+                key,
+                value,
+                interpolatedVariables: Array.map(extractInterpolatedVariables(value), (variable, i) => ({
+                    variable,
+                    keyForVariable: `OP2ENV_${id}_${key}_${i}`,
+                })),
+            })),
+            Array.map((a) => ({
+                ...a,
+                newValue: Array.reduce(a.interpolatedVariables, a.value, (acc, { variable, keyForVariable }) =>
+                    acc.replace(`\${${variable}}`, `\${${keyForVariable}}`),
+                ),
+            })),
+        )
 
-        const environmentVariablesFiltered = yield* Op.fetchSecretsAndListAllEnvironmentVariables(files).pipe(
-            Effect.map((ss) =>
+        const interpolatedVariables = pipe(
+            entriesWithInterpolatedVariables,
+            Array.map((e) => e.interpolatedVariables),
+            Array.flatten,
+            Array.map((v) => Tuple.make(v.keyForVariable, v.variable)),
+        )
+
+        const [entriesWithSomeInterpolatedVariables, entriesWithNoInterpolatedVariables] = Array.partition(
+            entriesWithInterpolatedVariables,
+            (a) => a.interpolatedVariables.length === 0,
+        )
+
+        const environmentVariablesToFetch = pipe(
+            interpolatedVariables,
+            Array.appendAll(
                 pipe(
-                    Array.filter(ss, (s) => {
-                        const t = s.split('=')
-                        if (!Tuple.isTupleOf(2)(t)) return false
-                        if (environmentVariablesNames.has(t[0])) return true
-                        return false
-                    }),
-                    Array.sort(Order.string),
+                    entriesWithNoInterpolatedVariables,
+                    Array.map(({ key, value }) => Tuple.make(key, value)),
                 ),
             ),
         )
 
-        yield* Console.log(environmentVariablesFiltered.join('\n'))
-    })
+        const combinedContents = pipe(
+            environmentVariablesToFetch,
+            Array.map((e) => e.join('=')),
+        ).join('\n')
 
-const command = Command.make('op2env-print', { files: Args.repeated(Args.file()) }, ({ files }) => program(files))
+        const fs = yield* FileSystem.FileSystem
+        const tempFile = yield* fs.makeTempFileScoped()
 
-const cli = Command.run(command, { name: 'TODO', version: '0.0.0' })
+        yield* fs.writeFileString(tempFile, combinedContents)
+
+        const environmentVariablesNames = new Set(Array.map(environmentVariablesToFetch, ([key]) => key))
+
+        const fetchedEnvironmentVariablesFiltered = yield* Op.fetchSecretsAndListAllEnvironmentVariables(tempFile).pipe(
+            Effect.map((ss) =>
+                pipe(
+                    ss,
+                    Array.map(splitByFirstEqualSign),
+                    Array.filter((a) => {
+                        if (a.length !== 2) return false
+                        if (environmentVariablesNames.has(a[0])) return true
+                        return false
+                    }),
+                    Array.map(([k, v]) => Tuple.make(k, v)),
+                ),
+            ),
+        )
+
+        const entriesWithNoInterpolatedVariablesKeys = new Set(
+            Array.map(entriesWithNoInterpolatedVariables, ({ key }) => key),
+        )
+        const [environmentVariablesForInterpolation, environmentVariablesWithNoInterpolatedVariables] = pipe(
+            fetchedEnvironmentVariablesFiltered,
+            Array.partition(([key]) => entriesWithNoInterpolatedVariablesKeys.has(key)),
+        )
+
+        const environmentVariablesWithSomeInterpolatedVariables = pipe(
+            entriesWithSomeInterpolatedVariables,
+            Array.map(({ key, newValue }) =>
+                Tuple.make(
+                    key,
+                    Array.reduce(environmentVariablesForInterpolation, newValue, (acc, a) =>
+                        acc.replace(`$\{${a[0]}}`, a[1]),
+                    ),
+                ),
+            ),
+        )
+
+        if (exprt) {
+            const lines = pipe(
+                environmentVariablesWithNoInterpolatedVariables,
+                Array.appendAll(environmentVariablesWithSomeInterpolatedVariables),
+                Array.map(([name, value]) => `export ${name}="${value}"`),
+            ).join('\n')
+
+            yield* Console.log(lines)
+        } else {
+            const environmentVariables = pipe(
+                environmentVariablesWithNoInterpolatedVariables,
+                Array.appendAll(environmentVariablesWithSomeInterpolatedVariables),
+                Array.map((a) => a.join('=')),
+            ).join('\n')
+
+            yield* Console.log(environmentVariables)
+        }
+    }).pipe(Effect.scoped)
+
+const command = Command.make(
+    'op2env-print',
+    { exprt: Options.boolean('export'), files: Args.repeated(Args.file()) },
+    ({ files, exprt }) => program(files, exprt),
+)
+
+const cli = Command.run(command, { name: 'op2env-print', version: '0.0.2' })
 
 Effect.suspend(() => cli(process.argv)).pipe(
     Effect.provide(NodeContext.layer),
